a9fad9a5df060d04b477496908be86aad700c68d
Timea Turdean <timea.turdean@gmail.com>
2022-04-29 22:29:33 +0200

    typo fix and comments deleted

--- formtest/test-form.js
+++ formtest/test-form.js
@@ -7,30 +7,6 @@ document.addEventListener('DOMContentLoaded', async function () {
 
   var dom = document
 
-  /* var uri = window.location.href
-  var base = (window.document.title = uri.slice(0, uri.lastIndexOf('/') + 1))
-  // var testDocURI = base + 'test.ttl' // imaginary doc - just use its URL
-  // const testDocURI = 'https://timbl.com/timbl/Public/Test/Forms/exampleData.ttl'
-  const testDocURI = 'https://solidos.solidcommunity.net/public/2021/10_example_data/example.ttl'
-  var testDoc = UI.rdf.sym(testDocURI)
-  const ex = UI.rdf.Namespace(testDocURI + "#") */
-
-  const defaultProlog = `
-  @prefix foaf:  <http://xmlns.com/foaf/0.1/>.
-  @prefix sched: <http://www.w3.org/ns/pim/schedule#>.
-  @prefix cal:   <http://www.w3.org/2002/12/cal/ical#>.
-  @prefix dc:    <http://purl.org/dc/elements/1.1/>.
-  @prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#>.
-  @prefix ui:    <http://www.w3.org/ns/ui#>.
-  @prefix trip:  <http://www.w3.org/ns/pim/trip#>.
-  @prefix vcard: <http://www.w3.org/2006/vcard/ns#>.
-  @prefix xsd:   <http://www.w3.org/2001/XMLSchema#>.
-  @prefix : <#>.
-  `
-  //@prefix ex: <#>.  # Things in the examples
-
-  // var div = dom.getElementById('UITestArea')
-
   const getOptions = {
     credentials: 'omit', withCredentials: false
   }
@@ -57,7 +33,7 @@ document.addEventListener('DOMContentLoaded', async function () {
     return str
   }
 
-  async function doRow (prolog, row) {
+  async function doRow (row) {
 
     async function loadTextIntoCell (cell) {
       const source = cell.getAttribute('source')
@@ -77,9 +53,6 @@ document.addEventListener('DOMContentLoaded', async function () {
     }
 
     const cellForClass = []
-
-    // UI.store.removeMany(null, null, null, testDoc) // Remove previous test data
-
 
     for (var cell of row.children) {
       await loadTextIntoCell(cell)
@@ -121,24 +94,14 @@ document.addEventListener('DOMContentLoaded', async function () {
     renderForm(form, subject, outputCell)
   }
   async function showResults () {
-    var prologEle = dom.getElementById('Prolog')
-    const prolog = defaultProlog + (prologEle ? prologEle.textContent : "")
-
+   
     const testRows = dom.getElementsByClassName('form-demo')
     for (var row of testRows) {
-      await doRow(prolog, row)
+      await doRow(row)
     }
-    if (dom.getElementById('TestData')) {
-      for (var row of dom.getElementById('TestData').children) {
-        await doRow(prolog, row)
-      }
-    }
+   
   } // showResults
 
-  try {
-    // await UI.store.fetcher.load(testDoc) // To fool the form syt
-  } catch (err) {
-    console.warn(err)
-  }
+
   await showResults()
 })
