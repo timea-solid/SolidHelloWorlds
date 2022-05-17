9cf3b15d72b76c9f960f1faa91381147028122e4
Timea Turdean <timea.turdean@gmail.com>
2022-05-17 12:15:49 +0200

    moved structural changes to a different branch

--- app.js
+++
@@ -1,7 +0,0 @@
-import * as UI from 'solid-ui'
-import * as solidUIcomponents from './components/facetedSearch/solid-ui-components.js'
-
-window.UI =  UI
-console.log("---import UI---")
-
-solidUIcomponents.solidUI.init()
