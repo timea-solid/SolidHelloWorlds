456eac467c17355d02c4a169b21cda107231bdd2
Timea Turdean <timea.turdean@gmail.com>
2022-07-14 11:48:23 +0200

    improved structure

--- src/facetedSearchPage/index.js
+++
@@ -1,9 +0,0 @@
-//solid-ui is used in solid-ui-components
-import * as UI from '../../lib/solid-ui'
-// for now we copy over the needed js from Jeff Zucker's project
-// see: "https://jeff-zucker.github.io/solid-ui-components/src/solid-ui-components.js
-import * as uiCom from '../../lib/solid-ui-components'
-
-uiCom.solidUI.init()
-
-document.getElementById("loading-span").hidden = true
