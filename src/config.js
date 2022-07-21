c2400985c1de8949ebf2f6e0f89598d86dbcdd02
Timea Turdean <timea.turdean@gmail.com>
2022-07-21 14:23:09 +0200

    switched to dev resources to make sure we don't loose the KG

--- src/config.js
+++ src/config.js
@@ -1,18 +1,17 @@
 //Note: appsDisplay.ttl, the form for the search interface has a hardcoded LINK_TO_KNOWLEDGE_GRAPH which has to be changed manually
-//TODO: make appsDisplay.ttl form pull link to KG from config
+//this are DEVELOPMENT configs and should be changed
 const appConfig = {
 
     // this is the resource we make snapshots for
-    LINK_TO_KNOWLEDGE_GRAPH: "https://timea.solidcommunity.net/HelloWorld/data/helloWorld.ttl",
+    LINK_TO_KNOWLEDGE_GRAPH: "https://timea.solidcommunity.net/SolidHelloWorldDevelopment/data/helloWorld.ttl",
     // this is the container where we place the snapshots
-    LINK_TO_KNOWLEDGE_GRAPH_SNAPSHOTS: "https://timea.solidcommunity.net/HelloWorld/data/snapshots/",
+    LINK_TO_KNOWLEDGE_GRAPH_SNAPSHOTS: "https://timea.solidcommunity.net/SolidHelloWorldDevelopment/data/snapshots/",
     // this is the basename of the created snapshots to which we add the timestamp and the filetype .ttl 
-    KNOWLEDGE_GRAPH_SNAPSHOT_NAME: "https://timea.solidcommunity.net/HelloWorld/data/snapshots/helloWorld",
+    KNOWLEDGE_GRAPH_SNAPSHOT_NAME: "https://timea.solidcommunity.net/SolidHelloWorldDevelopment/data/snapshots/helloWorld",
 
     //forms
-    LINK_TO_SEARCH_FORM: "../src/facetedSearchPage/appsDisplay.ttl",
     //solid-ui needs an absolute URL for the form
-    LINK_TO_EDIT_FORM: "https://solidweb.me/timeacss/public/SolidHelloWorlds/src/editDataPage/helloWorldForm.ttl",
+    LINK_TO_EDIT_FORM: "https://timea.solidcommunity.net/SolidHelloWorldDevelopment/forms/helloWorldForm.ttl",
 
     //SNAPSHOT related
     NUMBER_OF_SNAPSHOTS: 20
